cf2b540964c6a825a84f5bc8589ee5ce4c23aee2
Arrhythmia_V <78709808+Fennec82@users.noreply.github.com>
2026-05-01 17:35:11 -0400

    Revert "516 fixes (#3)"
    This reverts commit e883ada3474f2ab519ca2c0f4037f8c79064597c.

--- dependencies.sh
+++ dependencies.sh
@@ -5,13 +5,10 @@
 
 # byond version
 # Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
-#LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
-#export BYOND_MAJOR=${LIST[0]}
-#export BYOND_MINOR=${LIST[1]}
-#unset LIST
-
-export BYOND_MAJOR=516
-export BYOND_MINOR=1667
+LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
+export BYOND_MAJOR=${LIST[0]}
+export BYOND_MINOR=${LIST[1]}
+unset LIST
 
 #rust_g git tag
 export RUST_G_VERSION=0.4.3
